f340fda00c437b2e5e9b310c63b28cf32811f580
curs3dddd <lawaace52@gmail.com>
2026-02-21 07:34:30 -0800

    added a handler

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 )
 
 func main() {
+    // initialize the server stats
     mux := http.NewServeMux()
     server := &http.Server {
         Addr:       ":8080",
         Handler:    mux,
     }
 
+    // handlers
+    mux.Handle("/", http.FileServer(http.Dir(".")))
+    
+    // serve and listen to connections
     server.ListenAndServe()
 }

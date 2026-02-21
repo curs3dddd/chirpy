c03c3c2095ca8b968ceb060455793e453e541e24
curs3dddd <lawaace52@gmail.com>
2026-02-21 07:41:32 -0800

    Added logo

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 
     // handlers
     mux.Handle("/", http.FileServer(http.Dir(".")))
+    mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
     
     // serve and listen to connections
     server.ListenAndServe()
